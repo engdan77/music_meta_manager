e9947e9a8a8922c8f4224d93b737e36207291c5d
Daniel Engvall <daniel@engvalls.eu>
2022-02-08 21:10:59 +0100

    Add protocol for cast in BaseSong

--- music_manager/__main__.py
+++ music_manager/__main__.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Any, Annotated, Dict
+from typing import List, Tuple, Any, Annotated, Dict, Callable, Sequence, Iterable
 
 from pytunes.client import Client
 from loguru import logger
@@ -7,10 +7,9 @@ import pickle
 from abc import ABC, abstractmethod
 import xml.etree.ElementTree as ET
 from collections import namedtuple
-from dataclasses import dataclass, fields
+from dataclasses import dataclass, fields, field
 import datetime
 from typing import Optional
-
 
 @dataclass
 class BaseSong(ABC):
@@ -23,29 +22,37 @@ class BaseSong(ABC):
     bpm: int = 0
     played_count: int = 0
     rating: int = 0
-    date_added: datetime.date = datetime.date.today()
     year: int = datetime.date.today().year
+    date_added: '_normalize_datetime' = datetime.date.today()
 
     def __str__(self):
-        return f"{self.artist} - {self.name:<40} {self.year:<6} {'⭐️' * int(float(self.rating)/100 * 5) if self.rating else ''}"
+        return f"{self.artist} - {self.name:<40} {self.year:<6} {'⭐️' * int(float(self.rating) / 100 * 5) if self.rating else ''}"
 
     def __init__(self, **kwargs: Dict[Annotated[str, "Song field"], Annotated[Any, "Value"]]):
         for k, v in kwargs.items():
-            if k not in (_.name for _ in fields(self)):
+            normalized_field = self._normalize_field(k)
+            if normalized_field not in (_.name for _ in fields(self)):
                 continue
-            setattr(self, self._normalize_field(k), v)
+            setattr(self, normalized_field, self._cast(normalized_field, v))
+
+    def _cast(self, field_, value):
+        if func := next((_.type for _ in fields(self) if _.name == field_), None):
+            if not callable(func) and isinstance(func, str):
+                func = getattr(self, func)
+            return func(value)
+
 
     @staticmethod
     @abstractmethod
     def _normalize_field(
-        foreign_field_name: Annotated[str, "Field name to be converted"]
+            foreign_field_name: Annotated[str, "Field name to be converted"]
     ) -> Annotated[str, "Dataclass field name"]:
         """Take foreign field name and map to field in dataclass."""
 
     @staticmethod
     @abstractmethod
     def _normalize_datetime(
-        foreign_datetime: Annotated[str, "Datetime text to be converted"]
+            foreign_datetime: Annotated[str, "Datetime text to be converted"]
     ) -> datetime.datetime:
         """Take string and turn this into datetime."""
 
@@ -53,8 +60,7 @@ class BaseSong(ABC):
 class TunesSong(BaseSong):
 
     @staticmethod
-    def _normalize_field(foreign_field_name: Annotated[str, "Field name to be converted"]) -> Annotated[
-        str, "Dataclass field name"]:
+    def _normalize_field(foreign_field_name: Annotated[str, "Field name to be converted"]) -> Annotated[str, "Dataclass field name"]:
         fk = foreign_field_name.lower().replace(' ', '_')
         t = {'play_count': 'played_count'}
         return t.get(fk, fk)
@@ -65,9 +71,15 @@ class TunesSong(BaseSong):
 
 
 class SongsReader(ABC):
+    """Abstract base class for adapter reading songs from service"""
+
+    def __iter__(self):
+        for song in self.yield_song():
+            yield song
+
     @abstractmethod
-    def get_songs(self) -> List[BaseSong]:
-        pass
+    def yield_song(self) -> Iterable[BaseSong]:
+        """Return iterable for all songs"""
 
 
 class TunesFileReader(SongsReader):
@@ -85,9 +97,8 @@ class TunesFileReader(SongsReader):
         ]
         self.tree = ET.parse(xml).getroot()
 
-    def get_songs(self):
+    def yield_song(self):
         s = self.tree[0].findall("dict")[0]
-        songs = []
         for item in s:
             try:
                 if item[0].text == "Track ID":
@@ -98,10 +109,7 @@ class TunesFileReader(SongsReader):
             else:
                 keys_values_el: list[tuple[Any, Any]] = list(zip(keys, values))
                 key_values = {i[0].text: i[1].text for i in keys_values_el}
-                x = TunesSong(**key_values)
-                print(x)
-                songs.append(x)
-        return songs
+                yield TunesSong(**key_values)
 
 
 class MusicClientParser(SongsReader):
@@ -112,7 +120,7 @@ class MusicClientParser(SongsReader):
     def get_current_attribute(self, attribute):
         return self.c.current_track[attribute]
 
-    def get_songs(self):
+    def yield_song(self):
         songs = []
         rating_list = []
         self.jump_song(-1)
@@ -153,5 +161,6 @@ class MusicClientParser(SongsReader):
 
 if __name__ == "__main__":
     f = TunesFileReader()
-    all_songs = f.get_songs()
+    for s in f:
+        print(s)
     # m = MusicClientParser()
