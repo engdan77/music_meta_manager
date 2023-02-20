0d9ad12d241f23146ffa2f26ef1b108588a539a1
Daniel Engval <daniel@engvalls.eu>
2023-02-20 21:41:40 +0100

    Adding run entrypoints

---
+++ musicmanager/run_fix_location.py
@@ -0,0 +1,4 @@
+from musicmanager.cli import cli_fix_location
+
+if __name__ == '__main__':
+    cli_fix_location()
---
+++ musicmanager/run_migrate.py
@@ -0,0 +1,4 @@
+from musicmanager.cli import cli_migrate
+
+if __name__ == '__main__':
+    cli_migrate()
