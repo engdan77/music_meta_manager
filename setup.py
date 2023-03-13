938b86fb649447e5e5998866d55fa19bef1bfe66
Daniel Engvall <daniel.engvall@ipsoft.com>
2023-03-13 11:05:09 +0100

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+# from setuptools import setup
+from setuptools import setup
+with open("requirements.txt") as f:
+    dependencies = [line for line in f]
+
+setup(
+    name='music_meta_manager',
+    version='0.0.1',
+    packages=['musicmanager'],
+    url='',
+    license='',
+    author='edo',
+    author_email='daniel@engvalls.eu',
+    description='Tool for managing meta music information',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': ['music_migrate=musicmanager.cli:cli_migrate', 'music_fix_location=musicmanager.cli:cli_fix_location'],
+    }
+)
